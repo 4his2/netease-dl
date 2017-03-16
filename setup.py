e578818f7bd7c3cd28a402b00df661ed84da4372
ziwenxie <ziwenxie@outlook.com>
2017-03-16 23:52:09 +0800

    fix version error

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netease-dl',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
